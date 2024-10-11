95d2a89c3431761824aa968423218f8503fe14a1
Matheus Borges Teixeira <matheus.bt@gmail.com>
2024-10-12 00:09:25 +0200

    More puzzles.

--- 2024-10-11.kt
+++ 2024-10-11.kt
@@ -21,4 +21,29 @@ class `2024-10-11` {
             return if(min > nums.size) 0 else min
         }
     }
+
+    /* https://leetcode.com/problems/permutation-in-string/description/ */
+    class Solution567 {
+        fun checkInclusion(s1: String, s2: String): Boolean {
+            if (s1.length > s2.length) return false
+            val s1MapArray = IntArray(26) { 0 }
+            val s2MapArray = IntArray(26) { 0 }
+
+            for (i in s1.indices) {
+                s1MapArray[s1[i] - 'a']++
+                s2MapArray[s2[i] - 'a']++
+            }
+
+            var l = 0
+            var r = s1.length
+            while(r < s2.length) {
+                if (s1MapArray.contentEquals(s2MapArray)) return true
+                s2MapArray[s2[l] - 'a']--
+                s2MapArray[s2[r] - 'a']++
+                l += 1
+                r += 1
+            }
+            return s1MapArray.contentEquals(s2MapArray)
+        }
+    }
 }
