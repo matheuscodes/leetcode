878b8d2dd0c19ad4432b72853b5197aa90eaed38
Matheus Borges Teixeira <matheus.bt@gmail.com>
2024-12-02 00:05:57 +0100

    Best solution.

--- Problem32.kt
+++ Problem32.kt
@@ -4,14 +4,38 @@ class Problem32 {
     /* https://leetcode.com/problems/longest-valid-parentheses/description/ */
     class Solution {
 
-        // Runs 300x slower than the best solution on rank.
         fun longestValidParentheses(s: String): Int {
-            var processed = s
-            for(i in 0..s.length) {
-                val replacement = Array(i) { '*' }.joinToString(separator = "")
-                processed = processed.replace("($replacement)", "*$replacement")
+            var left = 0
+            var right = 0
+            var max = 0
+            for (i in s.indices) {
+                when (s[i]) {
+                    '(' -> left++
+                    ')' -> right++
+                }
+                if (left == right) {
+                    max = max(max, left * 2)
+                } else if (right > left) {
+                    left = 0
+                    right = 0
+                }
             }
-            return processed.split("(",")").map { it.length }.max() * 2
+
+            left = 0
+            right = 0
+            for (i in s.indices.reversed()) {
+                when (s[i]) {
+                    '(' -> left++
+                    ')' -> right++
+                }
+                if (left == right) {
+                    max = max(max, left * 2)
+                } else if (left > right) {
+                    left = 0
+                    right = 0
+                }
+            }
+            return max
         }
     }
 }
